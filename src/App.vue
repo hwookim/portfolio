0b83933a2934d0c591ed77236c2b2b37cea107ca
hwookim <gusdn9754@gmail.com>
2020-11-08 03:37:55 +0900

    feat: 정보란 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
   <div id="app">
-    <div class="px-4 py-10 pr-10">woo's portfolio</div>
+    <div class="flex h-screen bg-green-100 flex-col">
+      <div class="info flex w-4/5 mx-auto p-10 bg-white justify-between">
+        <div>
+          <div class="text-5xl font-bold">김현우</div>
+          <div>Backend Developer</div>
+        </div>
+        <div>
+          <div class="py-3">
+            <a href="https://github.com/hwookim" target="_blank"> hwookim </a>
+          </div>
+          <div class="py-3">
+            <a href="mailto:gusdn9754@gmail.com" target="_blank">
+              gusdn9754@gmail.com
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
