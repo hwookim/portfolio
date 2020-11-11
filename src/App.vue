be9de83f5065cb09ad80ddfdc070110ebd9b59d5
hwookim <gusdn9754@gmail.com>
2020-11-11 10:35:01 +0900

    feat: ToggleBox 구현

--- src/App.vue
+++ src/App.vue
@@ -38,72 +38,91 @@
         <content-box>
           <div class="ml-5 mb-3 text-2xl font-bold">About me</div>
           <div>
-            <div class="mb-3">
-              <div class="font-bold">테스트 작성을 당연하게 생각합니다.</div>
-              <div>
-                우아한테크코스에서 처음으로 웹 개발을 배우면서 항상 테스트가
-                있는 코드만을 접해왔습니다. Junit5를 통해 TDD 사이클의 개발을
-                자연스럽게 익혀왔습니다. 테스트가 없는 코드에 불안해하고
-                테스트를 통해 안정성을 보장하고 싶어합니다.
-              </div>
-            </div>
+            <toggle-box>
+              <template v-slot:title>
+                테스트 작성을 당연하게 생각합니다.
+              </template>
+              <template v-slot:content>
+                <li>
+                  우아한테크코스에서 처음으로 웹 개발을 배우면서 항상 테스트가
+                  있는 코드만을 접해왔습니다.
+                </li>
+                <li>
+                  Junit5를 통해 TDD 사이클의 개발을 자연스럽게 익혀왔습니다.
+                </li>
+                <li>
+                  테스트가 없는 코드에 불안해하고 테스트를 통해 안정성을
+                  보장하고 싶어합니다.
+                </li>
+              </template>
+            </toggle-box>
 
-            <div class="mb-3">
-              <div class="font-bold">
+            <toggle-box>
+              <template v-slot:title>
                 다양한 방법으로 의견을 공유하고 설득하고 설득당하는 걸 즐깁니다.
-              </div>
-              <div>팀원 간의 코드리뷰를 통해 더 나은 코드를 추구합니다.</div>
-              <div>
-                우아한 테크코스에서 운영하는 기술 블로그를 통해 배운 지식을
-                공유합니다.
-              </div>
-            </div>
+              </template>
+              <template v-slot:content>
+                <li>팀원 간의 코드리뷰를 통해 더 나은 코드를 추구합니다.</li>
+                <li>
+                  우아한 테크코스에서 운영하는 기술 블로그를 통해 배운 지식을
+                  공유합니다.
+                </li>
+              </template>
+            </toggle-box>
 
-            <div class="mb-3">
-              <div class="font-bold">
+            <toggle-box>
+              <template v-slot:title>
                 Spring framework를 통해 웹 서버를 구축할 수 있습니다.
-              </div>
-              <div>
-                우아한 테크코스에서 Spring Data JDBC와 JPA를 이용한 RestfulAPI
-                Application 구현 방법에 대해 학습했습니다.
-              </div>
-            </div>
+              </template>
+              <template v-slot:content>
+                <li>
+                  우아한 테크코스에서 Spring Data JDBC와 JPA를 이용한 RestfulAPI
+                  Application 구현 방법에 대해 학습했습니다.
+                </li>
+              </template>
+            </toggle-box>
 
-            <div class="mb-3">
-              <div class="font-bold">
+            <toggle-box>
+              <template v-slot:title>
                 Vue.js로 간단한 반응형 웹 페이지를 구현할 수 있습니다.
-              </div>
-              <div>현재 보고 계신 페이지를 템플릿 없이 직접 구현했습니다.</div>
-            </div>
+              </template>
+              <template v-slot:content>
+                <li>현재 보고 계신 페이지를 템플릿 없이 직접 구현했습니다.</li>
+              </template>
+            </toggle-box>
 
-            <div class="mb-3">
-              <div class="font-bold">
+            <toggle-box>
+              <template v-slot:title>
                 하나의 서비스에 필요한 최소한의 인프라 환경을 구축할 수
                 있습니다.
-              </div>
-              <div>
-                nginx를 통해 정적 웹 페이지 서버를 구축하고 원하는 환경을 설정할
-                수 있습니다.
-              </div>
-              <div>
-                Jenkins의 Publish over SSH와 Shell Script를 통해 Blue-Green
-                방식의 무중단 배포 환경을 구축했습니다.
-              </div>
-            </div>
+              </template>
+              <template v-slot:content>
+                <li>
+                  nginx를 통해 정적 웹 페이지 서버를 구축하고 원하는 환경을
+                  설정할 수 있습니다.
+                </li>
+                <li>
+                  Jenkins의 Publish over SSH와 Shell Script를 통해 Blue-Green
+                  방식의 무중단 배포 환경을 구축했습니다.
+                </li>
+              </template>
+            </toggle-box>
 
-            <div class="mb-3">
-              <div class="font-bold">
+            <toggle-box>
+              <template v-slot:title>
                 더 나은 방법, 더 나은 가치를 향해 노력합니다.
-              </div>
-              <div>
-                개발 도중 막히는 부분이 생기면 밤을 새서라도 해결하고
-                싶어합니다.
-              </div>
-              <div>최근에는 DDD와 Vue3에 관심이 생겨 학습하는 중입니다.</div>
-              <div>
-                1일 1커밋을 통해 꾸준히 성장하는 자세를 잃지 않으려고 합니다.
-              </div>
-            </div>
+              </template>
+              <template v-slot:content>
+                <li>
+                  개발 도중 막히는 부분이 생기면 밤을 새서라도 해결하고
+                  싶어합니다.
+                </li>
+                <li>최근에는 DDD와 Vue3에 관심이 생겨 학습하는 중입니다.</li>
+                <li>
+                  1일 1커밋을 통해 꾸준히 성장하는 자세를 잃지 않으려고 합니다.
+                </li>
+              </template>
+            </toggle-box>
           </div>
         </content-box>
 
@@ -164,9 +183,10 @@
 <script>
 import ContentBox from '@/component/ContentBox.vue';
 import EducationItem from '@/component/EducationItem.vue';
+import ToggleBox from '@/component/ToggleBox.vue';
 
 export default {
   name: 'App',
-  components: { EducationItem, ContentBox },
+  components: { ToggleBox, EducationItem, ContentBox },
 };
 </script>
---
+++ src/component/ToggleBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mb-3">
+    <div class="flex font-bold" @click="toggle">
+      <div class="mr-2">{{ mark }}</div>
+      <slot name="title"></slot>
+    </div>
+    <div class="p-5 bg-gray-100" :class="{ hidden }">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+const NOT_OPENED = '▶︎';
+const OPENED = '▼';
+
+export default {
+  name: 'ToggleBox',
+  data() {
+    return {
+      mark: NOT_OPENED,
+      hidden: true,
+    };
+  },
+  methods: {
+    toggle() {
+      this.mark = this.mark === NOT_OPENED ? OPENED : NOT_OPENED;
+      this.hidden = !this.hidden;
+    },
+  },
+};
+</script>
